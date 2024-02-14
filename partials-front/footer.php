9aa08a446327b459e3de9d0a21434a252a1ca490
UmoyoPrintex <128974681+UmoyoPrintex@users.noreply.github.com>
2024-02-14 17:18:47 +0200

    Add files via upload

---
+++ partials-front/footer.php
@@ -0,0 +1,122 @@
+
+        <!-- footer begin -->
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4">
+                        <img src="images/logo/logo.png" class="logo-small" alt=""><br>
+						<div>
+                            <p style="text-align:justify; ">Umoyo Printex was founded in 2023, It deals with various of services which included 3D logo designing, Brochures, Banners, Business Profile, Quotation, Receipts, Delivery notes, Business cards, and Website Developement..</p>
+							</div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="widget widget_recent_post">
+                            <h3>Quick Links</h3>
+                            <ul>
+                                <li><a href="about.php">Abut Us</a></li>
+                                <li><a href="projects.php">Projects</a></li>
+                                <li><a href="service-1.php">Graphic Design</a></li>
+                                <li><a href="service-2.php">Branding</a></li>
+                                <li><a href="service-3.php">Website Design</a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <h3>Contact Us</h3>
+                        <div class="widget widget-address">
+                            <address>
+                                <span>Permanent House, Room 245, Second Floor, Along Cairo Road, Lusaka, Zambia.</span>
+								<span><strong>Phone:</strong><a href="tel:+260973857222">+26 0973.857.222</a></span>
+                                <span><strong>Fax:</strong>(+260) 972.222.112</span>
+                                <span><strong>Email:</strong><a href="mailto:umoyoprintex@gmail.com">umoyoprintex@gmail.com</a></span>
+                                <span><strong>Web:</strong><a href="#">http://umoyoprintex.com</a></span>
+                            </address>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="subfooter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-6">
+                            &copy; Copyright 2024 - Umoyo Printex Company Ltd Designed By - <b>Chamanga David </b>                 
+                        </div>
+                        <div class="col-md-6 text-right">
+                            <div class="social-icons">
+                                <a href="#"><i class="fa fa-facebook fa-lg"></i></a>
+								<a href="#"><i class="fa fa-instagram fa-lg"></i></a>
+                                <a href="#"><i class="fa fa-twitter fa-lg"></i></a>
+								
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
+        <!-- footer close -->
+
+        <a href="#" id="back-to-top"></a>
+    </div>
+
+
+
+    <!-- Javascript Files
+    ================================================== -->
+    <script src="js/jquery.min.js"></script>	
+    <script src="js/jpreLoader.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.isotope.min.js"></script>
+    <script src="js/easing.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/jquery.countTo.js"></script>
+    <script src="js/validation.js"></script>
+    <script src="js/wow.min.js"></script>
+    <script src="js/jquery.magnific-popup.min.js"></script>
+    <script src="js/enquire.min.js"></script>
+    <script src="js/jquery.stellar.min.js"></script>
+    <script src="js/designesia.js"></script>
+
+    <!-- RS5.0 Core JS Files -->
+    <script type="text/javascript" src="revolution/js/jquery.themepunch.tools.min.js?rev=5.0"></script>
+    <script type="text/javascript" src="revolution/js/jquery.themepunch.revolution.min.js?rev=5.0"></script>
+
+    <!-- RS5.0 Extensions Files -->
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.video.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.navigation.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.actions.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.migration.min.js"></script>
+    <script type="text/javascript" src="revolution/js/extensions/revolution.extension.parallax.min.js"></script>
+
+    <script>
+        jQuery(document).ready(function () {
+            jQuery("#revolution-slider").revolution({
+                sliderType: "standard",
+                sliderLayout: "fullscreen",
+                delay: 3500,
+                navigation: {
+                    arrows: { enable: true }
+                },
+                parallax: {
+                    type: "mouse",
+                    origo: "slidercenter",
+                    speed: 2000,
+                    levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50],
+                },
+                spinner: "off",
+                gridwidth: 1140,
+                gridheight: 600,
+                disableProgressBar: "on"
+            });
+        });
+    </script>
+
+
+</body>
+</html>
---
+++ partials-front/menu.php
@@ -0,0 +1,108 @@
+<?php include('config/constants.php'); ?> 
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Umoyo Printex Company Ltd</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="BuildPro is the most complete constructon and building website template">
+    <meta name="keywords" content="construction,multipurpose,onepage,responsive,minimal,bootstrap,theme">
+    <meta name="author" content="">
+	<link rel="icon" type="image/png" href="images/logo/printex-favicon.png">
+    <!--[if lt IE 9]>
+	<script src="js/html5shiv.js"></script>
+	<![endif]-->
+
+
+    <!-- CSS Files
+    ================================================== -->
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css">	
+	<link rel="stylesheet" href="css/jpreloader.css" type="text/css">
+    <link rel="stylesheet" href="css/animate.css" type="text/css">
+    <link rel="stylesheet" href="css/owl.carousel.css" type="text/css">
+    <link rel="stylesheet" href="css/magnific-popup.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <!-- custom background -->
+    <link rel="stylesheet" href="css/bg.css" type="text/css">
+
+    <!-- color scheme -->
+    <link rel="stylesheet" href="css/color.css" type="text/css" id="colors">
+
+    <!-- RS5.0 Main Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/settings.css">
+
+    <!-- RS5.0 Layers and Navigation Styles -->
+    <link rel="stylesheet" type="text/css" href="revolution/css/layers.css">
+    <link rel="stylesheet" type="text/css" href="revolution/css/navigation.css">
+</head>
+
+<body id="homepage">
+    <div id="wrapper">
+        <!-- header begin -->
+        <header>
+            <div class="info">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <div class="col"><span class="id-color"><i class="fa fa-map-marker"></i></span>Permanent House, Room 245, 2nd Floor,Along Cairo Road Lusaka, Zambia.</div>
+                            <div class="col"><span class="id-color"><i class="fa fa-phone"></i></span>(+260) 973.857.222  |  +260 972.222.112 </div>
+                        </div>
+                        <div class="col-md-4 text-right">
+                            <!-- social icons -->
+                            <div class="col social">
+                                <a href="https://www.facebook.com/UmoyoMicroLoans/"><i class="fa fa-facebook"></i></a>
+								<a href="https://instagram.com/umoyomicroloans?igshid=YmMyMTA2M2Y"><i class="fa fa-instagram"></i></a>
+                                <a href="mailto:umoyoprintex@gmail.com"><i class="fa fa-envelope-o"></i></a>
+                            </div>
+                            <!-- social icons close -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <!-- logo begin -->
+                        <div id="logo">
+                            <a href="index.php">
+                                <img class="logo" src="images/logo/logo-3.png" alt="">
+                                <img class="logo-2" src="images/logo/logo-3.png" alt="">
+                            </a>
+                        </div>
+                        <!-- logo close -->
+
+                        <!-- small button begin -->
+                        <span id="menu-btn"></span>
+                        <!-- small button close -->
+
+                        <!-- mainmenu begin -->
+                        <nav>
+                            <ul id="mainmenu">
+                                <li><a href="<?php echo SITEURL; ?>">Home</a></li>
+                                <li><a href="<?php echo SITEURL; ?>category.php">Categories</a></li>
+                                <li><a href="<?php echo SITEURL; ?>services.php">Services</a></li>
+                                <li><a href="<?php echo SITEURL; ?>about.php">About Us</a>
+                                    <ul>
+                                        <li><a href="<?php echo SITEURL; ?>about.php">Company</a></li>
+                                        <li><a href="<?php echo SITEURL; ?>about-history.php">History</a></li>
+                                        <li><a href="<?php echo SITEURL; ?>about-team.php">Team</a></li>
+                                    </ul>
+                                </li>
+                                <li><a href="<?php echo SITEURL; ?>contact.php">Contact</a></li>
+                                <li><a href="<?php echo SITEURL; ?>admin/login.php">Login</a>
+                                    <ul>
+                                        <li><a href="<?php echo SITEURL; ?>admin/login.php">Admin Login</a></li>                          
+                                    </ul>
+                                </li>
+                            </ul>
+                        </nav>
+                    <!-- mainmenu close -->
+
+                </div>
+            </div>
+        </header>
+        <!-- header close -->
